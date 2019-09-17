40cdbe8c5cfec500789db088b95aa2234767e22f
gigarisch <gigarisch@gmail.com>
2019-09-17 14:00:12 -0400

    First upload
    Created flask app with hello world

---
+++ Week3/hello world.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 13:55:17 2019
+
+@author: gordon.garisch
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route(’/hello’, methods=[’GET’])
+def hello_world():
+    return "Hello, world!"
+
+if __name__ == "__main__":
+    app.run(host=’0.0.0.0’, port=8080)

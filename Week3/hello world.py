f4cdce377a20fd511694a7e71faba75ceab0c60a
gigarisch <33759023+gigarisch@users.noreply.github.com>
2019-09-22 09:29:33 -0400

    Added printing

--- Week3/hello world.py
+++ Week3/hello world.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 
 @app.route("/hello", methods=["GET"])
 def hello_world():
+    print("What should I print?")
     return "Hello, world!"
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
